e1e1e9b2770d26db3a0d1f1d9257046ed1900270
JungHoon <meslee70@gmail.com>
2023-08-07 20:41:28 +0900

    :tada:Column CRUD 완성 Column위치 변경 개발 예정

---
+++ src/main/java/com/example/tenrello/column/service/ColumnService.java
@@ -0,0 +1,23 @@
+package com.example.tenrello.column.service;
+
+import com.example.tenrello.column.dto.ColumnRequestDto;
+import com.example.tenrello.column.dto.ColumnResponseDto;
+import com.example.tenrello.common.dto.ApiResponseDto;
+import com.example.tenrello.entity.ColumnEntity;
+import com.example.tenrello.entity.User;
+import com.example.tenrello.security.details.UserDetailsImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface ColumnService {
+
+    ResponseEntity<ApiResponseDto> createcolumn(Long boardId, ColumnRequestDto columnRequestDto);
+
+    ResponseEntity<ApiResponseDto> updateColumnName(Long columnId, ColumnRequestDto columnRequestDto, User user);
+
+    ResponseEntity<ApiResponseDto> deleteColumnName(Long columnId, UserDetailsImpl userDetails);
+
+    List<ColumnResponseDto> getBoardColumn(Long boardId);
+}
---
+++ src/main/java/com/example/tenrello/column/service/ColumnServiceImpl.java
@@ -0,0 +1,86 @@
+package com.example.tenrello.column.service;
+
+import com.example.tenrello.board.BoardRepository;
+import com.example.tenrello.column.dto.ColumnRequestDto;
+import com.example.tenrello.column.dto.ColumnResponseDto;
+import com.example.tenrello.column.repository.ColumnRepository;
+import com.example.tenrello.common.dto.ApiResponseDto;
+import com.example.tenrello.entity.Board;
+import com.example.tenrello.entity.ColumnEntity;
+import com.example.tenrello.entity.User;
+import com.example.tenrello.security.details.UserDetailsImpl;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class ColumnServiceImpl implements ColumnService{
+
+    private final BoardRepository boardRepository;
+    private final ColumnRepository columnRepository;
+
+
+    @Override
+    public ResponseEntity<ApiResponseDto> createcolumn(Long boardId, ColumnRequestDto columnRequestDto) {
+        Board board = findBoard(boardId);
+        ColumnEntity column = new ColumnEntity(board,columnRequestDto);
+        columnRepository.save(column);
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseDto("새 컬럼 등록 성공",HttpStatus.OK.value()));
+    }
+
+    @Override
+    @Transactional
+    public ResponseEntity<ApiResponseDto> updateColumnName(Long columnId, ColumnRequestDto columnRequestDto, User user) {
+        ColumnEntity column = columnRepository.findById(columnId).orElse(null);
+
+        if(column == null) {
+            throw new IllegalArgumentException("해당 컬럼이 존재하지 않습니다.");
+        }
+
+        //해당 유저Role이 Member도 아니고 Admin도 아닐 시 User에 Role추가되면 수정
+//        if() {
+//            throw new IllegalArgumentException("해당 보드의 유저가 아닙니다.");
+//        }
+        column.setTitle(columnRequestDto.getTitle());
+
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseDto("컬럼제목 수정 성공", HttpStatus.OK.value()));
+    }
+
+    @Override
+    public ResponseEntity<ApiResponseDto> deleteColumnName(Long columnId, UserDetailsImpl userDetails) {
+        ColumnEntity column = columnRepository.findById(columnId).orElse(null);
+
+        if(column == null) {
+            throw new IllegalArgumentException("해당 컬럼이 존재하지 않습니다.");
+        }
+
+        //유저 권한이 Admin이 아니라면 삭제하지 않기
+
+        columnRepository.delete(column);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponseDto("컬럼 삭제 성공",HttpStatus.OK.value()));
+    }
+
+    @Override
+    public List<ColumnResponseDto> getBoardColumn(Long boardId) {
+
+        List<ColumnResponseDto> columnResponseDtoList = columnRepository.findAllByboardId(boardId).stream().map(ColumnResponseDto::new).toList();
+
+
+        return columnResponseDtoList;
+    }
+
+
+    public Board findBoard(Long id) {
+        return boardRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 board는 존재하지 않습니다."));
+    }
+}
