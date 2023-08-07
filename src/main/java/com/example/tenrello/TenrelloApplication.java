b63e101802931f7c7d088303a27853121fb8e871
LEEMINWOO <mw7328@naver.com>
2023-08-07 14:35:21 +0900

    main

---
+++ src/main/java/com/example/tenrello/TenrelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.tenrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenrelloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tenrello/TenrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tenrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenrelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
