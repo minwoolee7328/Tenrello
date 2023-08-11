4fd00ddc458cc46999f6e6e433f1cfd76b51f3c7
Minji-Sohn2 <cyzh1311@gmail.com>
2023-08-11 10:08:54 +0900

    :sparkles: 회원 탈퇴 프론트 구현, js로 분리

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -1,3 +1,62 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const goBackButton = document.getElementById('goBack');
+    const backToTopButton = document.getElementById('backToTop');
+
+    const changeNicknameButton = document.getElementById('changeNickname');
+    const changePasswordButton = document.getElementById('changePassword');
+    const deleteUserButton = document.getElementById('deleteUserBtn');
+
+    const closeNicknameModalButton = document.getElementById('closeNicknameModal');
+    const closePasswordModalButton = document.getElementById('closePasswordModal');
+    const closeDeleteUserModalButton = document.getElementById('closeDeleteUserModal');
+
+
+    goBackButton.addEventListener('click', function () {
+        window.history.back();
+    });
+
+    backToTopButton.addEventListener('click', function () {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    });
+
+    window.addEventListener('scroll', function () {
+        backToTopButton.style.display = (window.scrollY > 20) ? 'block' : 'none';
+    });
+
+    // 모달 창 띄우기
+    changeNicknameButton.addEventListener('click', showModal.bind(null, 'registerNicknameModal', 'nicknameModalOverlay'));
+    changePasswordButton.addEventListener('click', showModal.bind(null, 'registerPasswordModal', 'passwordModalOverlay'));
+    deleteUserButton.addEventListener('click', showModal.bind(null, 'registerDeleteUserModal', 'deleteUserModalOverlay'));
+
+    // 모달 창 닫기
+    closeNicknameModalButton.addEventListener('click', closeModal.bind(null, 'registerNicknameModal', 'nicknameModalOverlay'));
+    closePasswordModalButton.addEventListener('click', closeModal.bind(null, 'registerPasswordModal', 'passwordModalOverlay'));
+    closeDeleteUserModalButton.addEventListener('click', closeModal.bind(null, 'registerDeleteUserModal', 'deleteUserModalOverlay'));
+
+    // 닉네임 변경하기
+    $(document).on("click", "#submitNicknameModal", changeNickname);
+
+    // 비밀번호 변경하기
+    $(document).on("click", "#submitPasswordModal", changePassword);
+
+    // 회원 탈퇴하기
+    $(document).on("click", "#submitDeleteUserModal", deleteUser);
+});
+
+function showModal(modalId, overlayId) {
+    const modal = document.getElementById(modalId);
+    const overlay = document.getElementById(overlayId);
+    modal.style.display = 'block';
+    overlay.style.display = 'block';
+}
+
+function closeModal(modalId, overlayId) {
+    const modal = document.getElementById(modalId);
+    const overlay = document.getElementById(overlayId);
+    modal.style.display = 'none';
+    overlay.style.display = 'none';
+}
+
 function changeNickname() {
     let nicknameInput = $('#nicknameInput').val();
 
@@ -55,7 +114,7 @@ function changePassword() {
         data: JSON.stringify(data),
         success: function (xhr) {
             console.log(xhr);
-            alert(xhr.msg + '\n 다시 로그인 하세요.');
+            alert(xhr.msg + '\n다시 로그인 하세요.');
 
             // 변경 성공 > 로그아웃 ajax 요청
             $.ajax({
@@ -64,8 +123,7 @@ function changePassword() {
                 contentType: "application/json",
                 success: function (xhr) {
                     console.log(xhr.msg);
-                    Cookies.remove('Authorization', {path: '/'});
-                    window.location.href = "/view/main";
+                    logout();
                 },
                 error: function(response) {
                     console.log('변경 후 로그아웃 실패');
@@ -79,4 +137,39 @@ function changePassword() {
             alert(response.responseJSON.msg);
         }
     })
+}
+
+function deleteUser() {
+    let deleteUserInput = $('#deleteUserInput').val();
+
+    if (deleteUserInput.trim() === '') {
+        alert('비밀번호를 입력하세요');
+        return;
+    }
+
+    let data = {
+        password:deleteUserInput
+    };
+
+    $.ajax({
+        type: "DELETE",
+        url: `/api/users`,
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function (xhr) {
+            console.log(xhr);
+            alert(xhr.msg + '\n로그인 화면으로 돌아갑니다');     // 탈퇴 완료
+            logout();
+        },
+        error: function (response) {
+            console.log('회원 탈퇴 실패');
+            console.log(response.responseJSON.msg);         // 비밀번호가 일치하지 않습니다.
+            alert(response.responseJSON.msg);               // 비밀번호가 일치하지 않습니다.
+        }
+    })
+}
+
+function logout() {
+    Cookies.remove('Authorization', {path: '/'});
+    window.location.href = "/view/main";
 }
