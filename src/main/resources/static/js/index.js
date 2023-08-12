7991f30284566be08256becca2492acb4aa23853
minji-sohn2 <cyzh1311@gmail.com>
2023-08-12 20:54:19 +0900

    :art: 보드 전환, 멤버 리스트 보기 오류 (제외하고 모두 가능)

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,14 +1,11 @@
-<!--  offcanvas  -->
-
-
 const jwtToken = getJwtFromCookie();
 $(document).ready(function () {
     console.log("(document).ready");
 
 })
 
-
-function DeleteColumnBtn(id){
+// 컬럼 삭제
+function DeleteColumnBtn(id) {
     $.ajax({
         url: `/api/columns/${id}`, // 요청을 보낼 서버의 URL
         method: 'DELETE', // 요청 메소드 (GET, POST 등)
@@ -16,42 +13,45 @@ function DeleteColumnBtn(id){
             "Authorization": jwtToken,
         },
         success: function (response) {
-            alert("메뉴 삭제 완료")
+            alert("컬럼 삭제 완료")
             showSelectedBoard();
         },
         error: function (xhr, status, error) {
-            alert("메뉴 삭제 실패")
+            alert("컬럼 삭제 실패")
             console.log(xhr);
         }
     });
 
 }
-function modifyColumnBtn(id){
+
+// 컬럼 수정
+function modifyColumnBtn(id) {
 
     const modifyDiv = document.getElementById("columnModify");
     console.log(modifyDiv);
-    localStorage.setItem("modifyColumnId",id);
+    localStorage.setItem("modifyColumnId", id);
 
     modifyDiv.style.display = 'block';
 
 }
-    function getJwtFromCookie() {
-        const cookieName = 'Authorization'; // JWT가 저장된 쿠키의 이름
-        const cookies = document.cookie.split(';');
 
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
+function getJwtFromCookie() {
+    const cookieName = 'Authorization'; // JWT가 저장된 쿠키의 이름
+    const cookies = document.cookie.split(';');
 
-            if (cookie.startsWith(`${cookieName} = `)) {
-                const jwtCookie = cookie.substring(cookieName.length + 1);
-                return jwtCookie;
-            }
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+
+        if (cookie.startsWith(`${cookieName} = `)) {
+            const jwtCookie = cookie.substring(cookieName.length + 1);
+            return jwtCookie;
         }
-
-        return null; // JWT가 존재하지 않는 경우 null 반환
     }
 
-    //더미 데이터로 들어오는 컬럼 혹은 카드들을 움직이게 이벤트 설정 해주는 코드
+    return null; // JWT가 존재하지 않는 경우 null 반환
+}
+
+//더미 데이터로 들어오는 컬럼 혹은 카드들을 움직이게 이벤트 설정 해주는 코드
 document.addEventListener("DOMContentLoaded", function () {
     var offcanvasElement = document.querySelector("#offcanvasScrolling");
     var offcanvas = new bootstrap.Offcanvas(offcanvasElement);
@@ -118,19 +118,20 @@ lists.forEach(list => {
 });
 
 
-
 const closeNicknameModalButton = document.getElementById('closeNicknameModal');
 const registerNicknameModal = document.getElementById('registerNicknameModal');
 const nicknameModalOverlay = document.getElementById('nicknameModalOverlay');
 addListButton.addEventListener('click', () => {
 
-        registerNicknameModal.style.display = 'block';
-        nicknameModalOverlay.style.display = 'block';
+    registerNicknameModal.style.display = 'block';
+    nicknameModalOverlay.style.display = 'block';
 });
 closeNicknameModalButton.addEventListener('click', function () {
     registerNicknameModal.style.display = 'none';
     nicknameModalOverlay.style.display = 'none';
 });
+
+// 보드 속 컬럼이 존재하는지 여부에 따라 보이는 내용이 다르도록
 function toggleElements(responseExists) {
     if (responseExists) {
         // 컬럼이 있을 때
@@ -144,11 +145,12 @@ function toggleElements(responseExists) {
     }
 }
 
-function showSelectedBoard(boardId) {
+// 선택한 보드 보기
+function showSelectedBoard(boardId, boardTitle) {
 
     var clickedBoardId = localStorage.getItem("boardId");
 
-    var clickedBoardName = $(this).text();
+    var clickedBoardName = localStorage.getItem("boardTitle");
 
     console.log('Clicked Board ID:', clickedBoardId);
     console.log('Clicked Board Name:', clickedBoardName);
@@ -180,7 +182,7 @@ function showSelectedBoard(boardId) {
                 let cardTitles = a['cardList'];
 
                 columnSettingArr[i] = columnId;
-                i+=1;
+                i += 1;
 
                 let temp_html = `
                         <div class="list" id="${columnId}" draggable="true">
@@ -190,13 +192,13 @@ function showSelectedBoard(boardId) {
                             </div>
                     `;
 
-                cardTitles.forEach((a)=>{
-                    let card_html =`<div id="card-${a['id']}" class="card" draggable="true" onclick="model(${a['id']})">${a['title']}</div>`;
+                cardTitles.forEach((a) => {
+                    let card_html = `<div id="card-${a['id']}" class="card" draggable="true" onclick="model(${a['id']})">${a['title']}</div>`;
 
-                    temp_html+=card_html;
+                    temp_html += card_html;
                 })
 
-                temp_htmls += temp_html +`
+                temp_htmls += temp_html + `
                                              <div id="cardInsertDiv-${columnId}" class="insertDid" style="display: none">
                                              <input id="cardInsert-${columnId}"  type="text"/>
                                              <button id="insert" onclick="insertData(${columnId},${a['id']})">확인</button>
@@ -210,9 +212,11 @@ function showSelectedBoard(boardId) {
             });
             console.log('forEach문 끝');
             $('#listContainer').append(temp_htmls);
+
+            showMembersOfBoard(clickedBoardId);
             modalBoard.style.display = "block";
 
-            columnSettingArr.forEach((col)=>{       //조회된 컬럼들 순서대로 드래그 자리 바꾸기 가능하게 설정
+            columnSettingArr.forEach((col) => {       //조회된 컬럼들 순서대로 드래그 자리 바꾸기 가능하게 설정
                 const newList = document.getElementById(col);
                 newList.className = 'list';
                 newList.draggable = true;
@@ -250,26 +254,24 @@ function showSelectedBoard(boardId) {
                         if (newList.nextSibling === draggedItem.nextSibling) {
                             container.insertBefore(draggedItem, newList);
                             console.log("if");
-                        }
-                        else {
+                        } else {
                             var item = draggedItem;
-                            while(true){
+                            while (true) {
 
-                                if(item === null|| item.id === newList.id)
+                                if (item === null || item.id === newList.id)
                                     break;
-                                if(item.nextSibling ===null)
+                                if (item.nextSibling === null)
                                     item = null;
                                 else
-                                    item =item.nextSibling;
+                                    item = item.nextSibling;
 
                             }
-                            if(item === newList) {
+                            if (item === newList) {
                                 console.log("오른이동");
                                 // console.log("draggedItem"+draggedItem);
                                 // console.log("newList"+newList);
                                 container.insertBefore(draggedItem, newList.nextSibling);
-                            }
-                            else if(item === null){
+                            } else if (item === null) {
                                 // console.log("왼쪽이동");
                                 container.insertBefore(draggedItem, newList);
                             }
@@ -280,7 +282,7 @@ function showSelectedBoard(boardId) {
                             container.appendChild(list);
                         });
                         console.log("함수전");
-                        moveColumn(draggedItem.id,newList.id);
+                        moveColumn(draggedItem.id, newList.id);
                         newList.classList.remove('highlight');
                     }
                 });
@@ -290,13 +292,16 @@ function showSelectedBoard(boardId) {
         error: function () {
             // 보드 속 컬럼이 없을 때
             console.log('보드 속 컬럼 AJAX 요청 실패');
+
             toggleElements(false);
+            showMembersOfBoard(clickedBoardId);
         }
     });
 
     modalBoard.style.display = "block";
 }
-function moveColumn(dragColumn, targetColumn){
+
+function moveColumn(dragColumn, targetColumn) {
     console.log("moveColumn");
     $.ajax({
         type: "PUT",
@@ -312,15 +317,16 @@ function moveColumn(dragColumn, targetColumn){
         }
     })
 }
+
 function logout() {
     Cookies.remove('Authorization', {path: '/'});
     window.location.href = "/view/main";
 }
 
 //카드추가 버튼
-function createBtn(id){
+function createBtn(id) {
     //추가하기 버튼 해당 컬럼 id 불러오기
-    console.log("id",id);
+    console.log("id", id);
 
     $(`#listContainer`).find($(`div[class^='insertDid']`)).hide();
     $(`#listContainer`).find($(`button[class^='createBtn']`)).show();
@@ -336,9 +342,8 @@ function createBtn(id){
 }
 
 
-
 // 카드추가 취소
-function cancleBtn(id){
+function cancleBtn(id) {
     // 카드 추가하기 버튼 나타내기
     $(`#createBtn-${id}`).show();
 
@@ -347,7 +352,7 @@ function cancleBtn(id){
 }
 
 // 카드추가 기능
-function insertData(id, cardid){
+function insertData(id, cardid) {
 
     $.ajax({
         url: `/api/card/columns/${id}`,
@@ -379,7 +384,7 @@ function insertData(id, cardid){
 $("#cardName")
 
 
-function model(id){
+function model(id) {
     $(".cardModal").fadeIn();
 
     $("#cardName").text("카드이름");
@@ -404,7 +409,96 @@ function model(id){
 
 }
 
-function cardClose(){
+function cardClose() {
     $(".cardModal").fadeOut();
 }
 
+// 보드 모달 none, block
+const editBoardModal = document.getElementById('editBoardModal');
+const editClose = document.getElementById('editClose');
+const updateBoardButton = document.getElementById('updateBoard');
+
+updateBoardButton.addEventListener('click', () => {
+    editBoardModal.style.display = 'block';
+})
+
+editClose.addEventListener('click', () => {
+    editBoardModal.style.display = 'none';
+})
+
+const createBoardButton = document.getElementById('createBoard');
+const boardModalOverlay = document.getElementById('boardModalOverlay');
+const registerBoardModal = document.getElementById('registerBoardModal');
+const boardTitleInput = document.getElementById('boardTitle');
+const boardDescriptionInput = document.getElementById('boardDescription');
+const saveBoardButton = document.getElementById('saveBoard');
+const closeButton = document.getElementById('close');
+
+createBoardButton.addEventListener('click', () => {
+    boardModalOverlay.style.display = 'block';
+    registerBoardModal.style.display = 'block';
+});
+
+closeButton.addEventListener('click', () => {
+    boardModalOverlay.style.display = 'none';
+    registerBoardModal.style.display = 'none';
+});
+
+// 보드 삭제 함수
+function deleteThisBoard() {
+    // 지금 보이는 보드의 id, title
+    var boardIdToDelete = $(this).attr('data-board-id');
+    var boardNameToDelete = $(this).attr('data-board-name');
+
+    console.log(boardIdToDelete);
+
+    // alert 창으로 한 번 더 확인 후
+    if (confirm("정말 " + boardNameToDelete + "을(를) 삭제하시겠습니까?")) {
+        // 삭제 요청
+        $.ajax({
+            type: "DELETE",
+            url: `/api/boards/` + boardIdToDelete,
+            contentType: "application/json",
+            success: function (xhr) {
+                console.log(xhr.msg);                   // 삭제되었습니다.
+                alert(xhr.msg);                   // 삭제되었습니다.
+                location.reload();
+            },
+            error: function (response) {
+                console.log('보드 삭제 실패');
+                console.log(response.responseJSON.msg);
+                alert(response.responseJSON.msg);       // 관리자만 ~ 있습니다.
+            }
+        })
+    }
+}
+
+// 보드에 속한 멤버 보기
+function showMembersOfBoard(clickedBoardId) {
+    $.ajax({
+        url: 'http://localhost:8080/api/boards/' + clickedBoardId + '/members',
+        type: 'GET',
+        contentType: 'application/json',
+        headers: {
+            'Authorization': document.cookie
+        },
+        success: function (response) {
+            console.log(response);
+
+            const memberDropdownList = document.getElementById('dropdown-menu');
+            memberDropdownList.innerHTML = ''; // Clear any previous entries
+
+            response.memberList.forEach(member => {
+                const memberListItem = document.createElement('li');
+                const memberLink = document.createElement('a'); // Create an anchor element
+                memberLink.className = 'dropdown-item'; // Set the class name
+                memberLink.textContent = member.username; // Set the username as text content
+                memberListItem.appendChild(memberLink); // Append the anchor element to the list item
+                memberDropdownList.appendChild(memberListItem); // Append the list item to the dropdown menu
+            });
+        },
+        error: function () {
+            console.log('멤버 목록 불러오기 오류');
+        }
+    });
+}
